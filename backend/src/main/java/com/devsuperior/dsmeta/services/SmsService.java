e4b11a19a4d3d367dc893e813eabb05ef098c908
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2022-11-18 16:13:19 -0300

    Melhorando mensagem de notificacao

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dsmeta.services;
 
+import com.devsuperior.dsmeta.entities.Sales;
+import com.devsuperior.dsmeta.repositories.SalesRepositories;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +24,22 @@ public class SmsService {
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
 
-	public void sendSms() {
+	@Autowired
+	private SalesRepositories saleRepo;
+
+	public void sendSms(Long saleId) {
+		Sales sale = saleRepo.findById(saleId).get();
+
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+
+		String msg = "O vendedor: " + sale.getSellerName()
+				+ " vendeu R$" + sale.getAmount()
+				+ " no mês " + date;
+
 		Twilio.init(twilioSid, twilioKey);
 		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-		Message message = Message.creator(to, from, "Testando envio de SMS").create();
+		Message message = Message.creator(to, from, msg).create();
 		System.out.println(message.getSid());
 	}
 }
