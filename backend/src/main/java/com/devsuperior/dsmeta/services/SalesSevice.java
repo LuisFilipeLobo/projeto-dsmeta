fc56872efea628bd6a10d89dc9506adfd7de5cac
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2022-11-18 11:32:11 -0300

    Adicionando busca por datas

--- backend/src/main/java/com/devsuperior/dsmeta/services/SalesSevice.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SalesSevice.java
@@ -3,16 +3,28 @@ package com.devsuperior.dsmeta.services;
 import com.devsuperior.dsmeta.entities.Sales;
 import com.devsuperior.dsmeta.repositories.SalesRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SalesSevice {
     @Autowired
     private SalesRepositories repo;
 
-    public List<Sales> findSales() {
-        return repo.findAll();
+    public Page<Sales> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        // Criar variável com dia atual
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        // Converter as datas
+        LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repo.findSales(min, max, pageable);
     }
 }
